b37e434557d48eef5d441261a96c595e34753671
Christopher John Cucio <cuciocj@gmail.com>
2020-08-11 20:22:17 +1200

    added flutter_dotenv dependency for securing global variables

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_isitgood/routes/routes.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+Future main() async {
+  await DotEnv().load('.env');
   runApp(MyApp());
 }
 
