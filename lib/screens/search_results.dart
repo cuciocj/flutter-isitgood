a27e2d578c4ff7f64ebafd5ebb951cc6d0446c6a
Christopher John Cucio <cuciocj@gmail.com>
2020-08-13 23:40:57 +1200

    refactored search result

--- lib/screens/search_results.dart
+++ lib/screens/search_results.dart
@@ -2,8 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_isitgood/model/movie.dart';
 import 'package:flutter_isitgood/model/movie_list.dart';
 import 'package:flutter_isitgood/services/tmdb_api.dart';
-import 'package:flutter_isitgood/widgets/search_results/blurred_backdrop.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_isitgood/widgets/search_results/search_result_tile.dart';
 
 class SearchResults extends StatelessWidget {
   @override
@@ -37,95 +36,11 @@ class SearchResultsListState extends State<SearchResultsList> {
     movieList = APIProvider().findByTitle(widget.searchQuery);
   }
 
-  // TODO: refactor
-  Widget _buildSearchResultTile(BuildContext context, Movie movie) {
-    double listItemTileHeight = MediaQuery.of(context).size.width * 0.5;
-    
-    movie.backdropPath == null ?
-      print('backdrop is null') :
-      print(movie.backdropPath);
-
-    return Container(
-      height: listItemTileHeight,
-      child: Stack(
-        children: <Widget>[
-          BlurredBackdrop(movie.backdropPath),
-          Padding(
-            padding: EdgeInsets.fromLTRB(20, 15, 30, 15),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ClipRRect(
-                  borderRadius: BorderRadius.circular(15.0),
-                  child: Image(
-                    // TODO: avoid hardcoded string
-                    image: movie.posterPath == null ? 
-                      AssetImage('assets/images/poster_placeholder.png') :
-                      NetworkImage('http://image.tmdb.org/t/p/w780${movie.posterPath}'),
-                  ),
-                ),
-                Container(
-                  width: 250.0,
-                  child: Column(
-                    children: [
-                      Flexible(
-                        child: Container(
-                          margin: EdgeInsets.fromLTRB(15, 10, 0, 0),
-                          child: Text(
-                            movie.name,
-                            style: GoogleFonts.openSans(
-                              textStyle: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 30.0,
-                                color: Colors.white,
-                                shadows: [
-                                  Shadow(
-                                    blurRadius: 5.0,
-                                    color: Colors.black
-                                  )
-                                ]
-                              )
-                            ),
-                          ),
-                        ),
-                      ),
-                      Flexible( 
-                        child: Container(
-                          margin: EdgeInsets.fromLTRB(15, 10, 0, 0),
-                          // color: Colors.purple[200],
-                          child: Text(
-                            movie.overview,
-                            style: GoogleFonts.openSans(
-                              textStyle: TextStyle(
-                                fontSize: 15.0,
-                                color: Colors.white,
-                                shadows: [
-                                  Shadow(
-                                    blurRadius: 5.0,
-                                    color: Colors.black
-                                  )
-                                ]
-                              )
-                            ),
-                          ),
-                        ),
-                      )
-                    ],
-                  ),
-                )
-              ],
-            ),
-          )
-        ],
-      ),
-    );
-  }
-
   Widget _buildSearchResultList(BuildContext context, List<Movie> list) {
     return ListView.builder(
       itemCount: list.length,
       itemBuilder: (BuildContext context, int index) => 
-        _buildSearchResultTile(context, list[index])
+        SearchResultTile(movie: list[index])
     );
   }
 
@@ -136,20 +51,15 @@ class SearchResultsListState extends State<SearchResultsList> {
       builder: (context, snapshot) {
         if(snapshot.hasData) {
           return _buildSearchResultList(context, snapshot.data.list);
-        } else if(snapshot.hasError) {
-          return Text('${snapshot.error}');
+        } else if (snapshot.hasError) {
+          return Center(
+            child: Text('${snapshot.error}'),
+          );
         }
-        return CircularProgressIndicator();
+        return Center(
+          child: CircularProgressIndicator(),
+        );
       },
     );
   }
-
-  // @override
-  // Widget build(BuildContext context) {
-  //   return ListView(
-  //     children: <Widget>[
-  //       _buildSearchResultTile(context),
-  //     ]
-  //   );
-  // }
 }
---
+++ lib/widgets/search_results/search_result_tile.dart
@@ -0,0 +1,195 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_isitgood/model/movie.dart';
+import 'package:flutter_isitgood/widgets/search_results/blurred_backdrop.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/intl.dart';
+import 'package:percent_indicator/percent_indicator.dart';
+
+class SearchResultTile extends StatefulWidget {
+  final Movie movie;
+
+  const SearchResultTile({Key key, this.movie}) : super(key: key);
+
+  @override
+  SearchResultTileState createState() => SearchResultTileState();
+}
+
+class SearchResultTileState extends State<SearchResultTile> {
+
+  String transformStringDate(String strDate, 
+    String inputFormat, 
+    String outputFormat) {
+      DateTime dt = DateFormat(inputFormat).parse(strDate);
+      return DateFormat(outputFormat).format(dt);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    double listItemTileHeight = MediaQuery.of(context).size.width * 0.5;
+
+    return Container(
+      height: listItemTileHeight,
+      child: Stack(
+        children: <Widget>[
+          BlurredBackdrop(widget.movie.backdropPath),
+          Padding(
+            padding: EdgeInsets.fromLTRB(20, 15, 30, 15),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(15.0),
+                  child: Image(
+                    // TODO: avoid hardcoded string
+                    image: widget.movie.posterPath == null ? 
+                      AssetImage('assets/images/poster_placeholder.png') :
+                      NetworkImage(
+                        'http://image.tmdb.org/t/p/w780${widget.movie.posterPath}'
+                      ),
+                  ),
+                ),
+                Container(
+                  width: 250.0,
+                  // color: Colors.yellow,
+                  padding: EdgeInsets.only(left: 15, top: 10),
+                  child: Column(
+                    children: [
+                      Flexible(
+                        flex: 0,
+                        child: Container( // TODO: refactor movie title section
+                          width: double.infinity,
+                          // color: Colors.purple,
+                          child: Text(
+                            widget.movie.name,
+                            style: GoogleFonts.openSans(
+                              textStyle: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 30.0,
+                                color: Colors.white,
+                                shadows: [
+                                  Shadow(
+                                    blurRadius: 5.0,
+                                    color: Colors.black
+                                  )
+                                ]
+                              )
+                            ),
+                          ),
+                        ),
+                      ),
+                      Flexible(
+                        fit: FlexFit.tight,
+                        child: Container( // TODO: refactor movie release date
+                          width: double.infinity,
+                          // color: Colors.pink[200],
+                          child: Text(widget.movie.releaseDate != '' ?
+                            transformStringDate(
+                              '${widget.movie.releaseDate}',
+                              'yyyy-MM-dd',
+                              'MMMM dd, yyyy'
+                            ) : 'No release date',
+                            textAlign: TextAlign.left,
+                            style: GoogleFonts.openSans(
+                              textStyle: TextStyle(
+                                fontSize: 12.0,
+                                color: Colors.white70,
+                                shadows: [
+                                  Shadow(
+                                    color: Colors.black
+                                  )
+                                ]
+                              )
+                            ),
+                          ),
+                        ),
+                      ),
+                      Flexible(
+                        child: Container( // TODO: refactor genre list
+                          // color: Colors.purple,
+                          width: double.infinity,
+                          child: Text('Drama | Action | Comedy',
+                              style: GoogleFonts.openSans(
+                                textStyle: TextStyle(
+                                  fontSize: 12.0,
+                                  color: Colors.white70,
+                                  shadows: [
+                                    Shadow(
+                                      color: Colors.black87
+                                    )
+                                  ]
+                                )
+                              ),
+                            ),
+                        ),
+                      ),
+                      Flexible(
+                        child: Container( // TODO: refactor rating
+                          // color: Colors.green,
+                          alignment: Alignment.centerLeft,
+                          // transform: Matrix4.translationValues(0.0, 0.0, 0.0),
+                          child: LinearPercentIndicator(
+                            padding: EdgeInsets.only(left: 5, right: 10),
+                            width: MediaQuery.of(context).size.width / 5,
+                            lineHeight: 10.0,
+                            percent: widget.movie.voteAverage / 10,
+                            // progressColor: Colors.lightGreen,
+                            linearGradient: LinearGradient(
+                              colors: [
+                                Color.fromRGBO(95,160,237, 1),
+                                Color.fromRGBO(237,136,181, 1)
+                              ]
+                            ),
+                            trailing: Text(
+                              '${widget.movie.voteAverage.toStringAsFixed(1)}',
+                              style: GoogleFonts.openSans(
+                                textStyle: TextStyle(
+                                  fontSize: 12.0,
+                                  color: Colors.white70,
+                                  shadows: [
+                                    Shadow(
+                                      blurRadius: 2.0,
+                                      color: Colors.black87
+                                    )
+                                  ]
+                                )
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Flexible( // TODO: refactor overview section; fill remaning height
+                        flex: 4,
+                        fit: FlexFit.tight,
+                        child: SingleChildScrollView(
+                          child: Container(
+                            // color: Colors.green,
+                            child: Text(
+                              widget.movie.overview,
+                              style: GoogleFonts.openSans(
+                                textStyle: TextStyle(
+                                  fontSize: 15.0,
+                                  color: Colors.white70,
+                                  shadows: [
+                                    Shadow(
+                                      blurRadius: 5.0,
+                                      color: Colors.black87
+                                    )
+                                  ]
+                                )
+                              ),
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+}
